fbaf9cc8e4ae709e270d416eb17951901b7aa78c
heinrich27 <hendrik-horstmann@o2mail.de>
2022-05-10 11:51:17 +0200

    lets go

---
+++ src/main/kotlin/Fach.kt
@@ -0,0 +1,1 @@
+data class Fach(val name: String, val kuerzel: String, val aufgabenfeld: Int)
---
+++ src/main/kotlin/fremdsprachen.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class fremdsprachen {
+    private JComboBox sprache1;
+    private JPanel panel1;
+    private JComboBox sprache2;
+    private JComboBox sprache3;
+    private JComboBox start1;
+    private JComboBox start2;
+    private JComboBox start3;
+}
