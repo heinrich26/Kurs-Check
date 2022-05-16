6639d30a34b4c0f12117664e7fd4d11adf731d64
heinrich27 <hendrik-horstmann@o2mail.de>
2022-05-16 12:14:23 +0200

    abstrakte klasse hinzugefügt

--- src/main/kotlin/gui/kurswahlPanel.kt
+++ src/main/kotlin/gui/kurswahlPanel.kt
@@ -2,8 +2,32 @@ package gui
 
 import data.FachData
 import data.KurswahlData
+import javax.swing.JFrame
 import javax.swing.JPanel
+import javax.swing.UIManager
 
 abstract class KurswahlPanel(val wahlData: KurswahlData, val fachData: FachData) : JPanel() {
     abstract fun close() : KurswahlData
+
+    abstract val windowName: String
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            try {
+                UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel")
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+            }
+
+            val panel = JPanel()
+            val frame = JFrame()
+            frame.contentPane = panel
+
+            frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+            frame.setSize(300, 300)
+            frame.setLocation(430, 100)
+            frame.isVisible = true
+        }
+    }
 }
