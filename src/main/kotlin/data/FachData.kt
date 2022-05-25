4415e6778db89d8f0a78e1b61b47a24aa2961cde
henrich26 <hendrik-horstmann@o2mail.de>
2022-05-25 15:26:50 +0200

    Wahlzeilenmatching weitergeführt

--- src/main/kotlin/data/FachData.kt
+++ src/main/kotlin/data/FachData.kt
@@ -23,16 +23,22 @@ class FachData(
      */
     fun filterWahlzeilen(lk1: Fach?, lk2: Fach?, pf3: Fach?, pf4: Fach?, pf5: Fach?): Map<Int, Wahlzeile> {
         val predicates = mutableListOf<(Wahlzeile) -> Boolean>()
-        if (lk1 != null) predicates.add { it.lk1 in wildcardMapping[lk1]!!}
-        if (lk2 != null) predicates.add { it.lk2 in wildcardMapping[lk2]!!}
-        if (pf3 != null) predicates.add { it.pf3 in wildcardMapping[pf3]!!}
-        if (pf4 != null) predicates.add { it.pf4 in wildcardMapping[pf4]!!}
-        if (pf5 != null) predicates.add { it.pf5 in wildcardMapping[pf5]!!}
+        if (lk1 != null) predicates.add { it.lk1 == "*" || it.lk1 in wildcardMapping[lk1]!! }
+        if (lk2 != null) predicates.add { it.lk2 == "*" || it.lk2 in wildcardMapping[lk2]!! }
+        if (pf3 != null) predicates.add { it.pf3 == "*" || it.pf3 in wildcardMapping[pf3]!! }
+        if (pf4 != null) predicates.add { it.pf4 == "*" || it.pf4 in wildcardMapping[pf4]!! }
+        if (pf5 != null) predicates.add { it.pf5 == "*" || it.pf5 in wildcardMapping[pf5]!! }
 
         if (predicates.isEmpty()) return wahlzeilen
 
-        return wahlzeilen.filterValues { wz -> predicates.all { it(wz) } }
+        return wahlzeilen.filterValues { zeile -> predicates.all { it(zeile) } }
     }
+
+    /**
+     * Gibt alle möglichen Wahlzeilen für die gegebenen LKs zurück
+     */
+    fun filterWahlzeilen(data: KurswahlData): Map<Int, Wahlzeile> =
+        filterWahlzeilen(data.lk1, data.lk2, data.pf3, data.pf4, data.pf5)
 
     @Deprecated("Ineffizient und brauch ich halt ned")
     fun matchField(fach: Fach, selector: String): Boolean =
