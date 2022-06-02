b28eb6baf83561eebe036b89d184528ea4448d86
heinrich27 <hendrik-horstmann@o2mail.de>
2022-06-02 08:21:36 +0200

    weiterarbeiten an PDF-Bearbeitung (siehe pdfTest.kt)

--- src/main/kotlin/gui/GrundkursWahl.kt
+++ src/main/kotlin/gui/GrundkursWahl.kt
@@ -152,24 +152,22 @@ class GrundkursWahl(wahlData: KurswahlData, fachData: FachData) : KurswahlPanel(
      */
     private fun buildCheckboxes() {
         // fremdsprachen & wpfs holen
+        val fs = wahlData.fremdsprachen.map { it.first }
+        println(wahlData.fremdsprachen)
+        val wpfs = wahlData.wpfs
+
         for ((i, fach) in fachData.faecher.withIndex()) {
-            // TODO alle Fremdsprachen, die nicht in fachData.fremdsprachen sind, verstecken
-            // TODO alle WPFs Fächer, die nicht in fachData.wpfs sind, verstecken
+            // TODO alle Fremdsprachen, die nicht in wahlData.fremdsprachen sind, verstecken
+            // TODO alle WPFs Fächer, die nicht in wahlData.wpfs sind, verstecken
             // TODO das Label brauchst du dann nicht zu erstellen, nur die Checkbox (ohne listener etc.)
 
             // TODO Condition aus Leistungskurse.kt... mach dir eine Zwischenvariable, weil effizienz
-            /*
-            Das kommt außerhalb des for-Loops :)
-            val fs = fachData.fremdsprachen
-            val wpfs = fachData.wpfs
-
-            (!it.fremdsprache || it in fs) &&
+            val cond = (!fach.fremdsprache || fach in fs) &&
                     /* Hat keine WPF or Fach ist weder 1./2. WPF */
-                    (!it.brauchtWPF || (wpfs != null && (it == wpfs.first || it == wpfs.second)))
-            */
+                    (!fach.brauchtWPF || (wpfs != null && (fach == wpfs.first || fach == wpfs.second)))
 
 
-            if (true /* TODO hier kommt deine Zwischenvariable rein ;) */) {
+            if (cond /* TODO hier kommt deine Zwischenvariable rein ;) */) {
                 val label = JLabel(fach.name)
 
                 // Wenn man das Label anklickt wird die ganze Zeile ausgewählt
@@ -204,7 +202,7 @@ class GrundkursWahl(wahlData: KurswahlData, fachData: FachData) : KurswahlPanel(
             for (j in 1..4) {
                 val box = JCheckBox()
 
-                if (true /* TODO hier kommt deine Zwischenvariable rein ;) */) {
+                if (cond /* TODO hier kommt deine Zwischenvariable rein ;) */) {
                     box.isOpaque = false
                     box.addActionListener { if ((it.source as JToggleButton).isSelected) anzahl++ else anzahl-- }
                 } else box.isVisible = false
--- src/main/kotlin/pdfTest.kt
+++ src/main/kotlin/pdfTest.kt
@@ -2,6 +2,8 @@ import com.itextpdf.forms.PdfAcroForm
 import com.itextpdf.kernel.pdf.PdfDocument
 import com.itextpdf.kernel.pdf.PdfReader
 import com.itextpdf.kernel.pdf.PdfWriter
+import data.Fach
+import data.Wahlmoeglichkeit
 
 
 fun main() {
@@ -13,12 +15,43 @@ fun main() {
     val fields = stamper.formFields
     val keys = fields.keys
 
-    println(keys.joinToString("', '", "'", "'"))
+    println(keys.joinToString("', '", "'", "'")) // gibt namen der Felder aus
 
     stamper.replaceField(
         "Name, Vornamen (alle!)",
         fields["Name, Vornamen (alle!)"]!!.setValue("Kurek, Togert Tiburtius")
     )
+
+    val data = testKurswahl
+
+//    val mapping = mapOf("Ma" to listOf("PF Ma", "Q1 Ma", "Q2 Ma", "Q3 Ma", "Q4 Ma"))
+    val mapping = data.kurse.keys.associateWith { kurs -> arrayOf("PF ${kurs.kuerzel}", "Q1 ${kurs.kuerzel}", "Q2 ${kurs.kuerzel}") }
+
+    val startIndicies = mapOf(1 to 0, 2 to 8, 3 to 13)
+
+    val agfelder = mutableMapOf<Int, MutableList<Pair<Fach, Wahlmoeglichkeit>>>()
+    for ((fach, wmoegl) in data.kurse.entries) {
+        val feld = fach.aufgabenfeld
+        if (!agfelder.containsKey(feld))
+            agfelder[feld] = mutableListOf(fach to wmoegl)
+        else
+            agfelder[feld]!!.add(fach to wmoegl)
+    }
+
+        data.gks.onEachIndexed { i, (fach, wmoegl) ->
+            stamper.replaceField("$i $0", fields["$i $0"]!!.setValue(fach.name))
+            // TODO kreuze machen
+        }
+//    for ((fach, wahlmoegl) in ) {
+//        when (wahlmoegl) {
+//            data.Wahlmoeglichkeit.ERSTES_ZWEITES -> // 1, 2
+//            data.Wahlmoeglichkeit.ERSTES_DRITTES -> // 1, 2, 3
+//            data.Wahlmoeglichkeit.ZWEITES_VIERTES -> // ..
+//            data.Wahlmoeglichkeit.DRITTES_VIERTES -> TODO()
+//            data.Wahlmoeglichkeit.DURCHGEHEND -> TODO()
+//        }
+//    }
+
 
     document.close()
 }
