f99330520fca94694d6e989a9e3fb7daf28e9174
heinrich26 <hendrik-horstmann@o2mail.de>
2022-06-03 17:39:23 +0200

    Prüfungsfachwahl kann jz 3. & 4.
    TestDaten können nun beim Ausführen verwendet werden, wenn auch seeehr Buggy

--- src/main/kotlin/TestData.kt
+++ src/main/kotlin/TestData.kt
@@ -1,10 +1,12 @@
-import data.*
+import data.Fach
+import data.KurswahlData
+import data.Wahlmoeglichkeit
 
-val eng = Fach("Englisch", "E", 1, true)
+val eng = Fach("Englisch", "En", 1, true)
 val ma = Fach("Mathe", "Ma", 2, true)
 val de = Fach("Deutsch", "De", 1, true)
 val inf = Fach("Informatik", "Inf", 2, true)
-val geo = Fach("Geographie", "Geo", 3, true)
+val geo = Fach("Geografie", "Geo", 3, true)
 val sp = Fach("Sport", "Sp", 0, false)
 val ge = Fach("Geschichte", "Ge", 3, true)
 val spa = Fach("Spanisch", "Spa", 1, true)
@@ -15,17 +17,12 @@ val fremdsprachen = listOf(eng, spa, lat, fr)
 var faecher = listOf(eng, ma, de, inf, geo, sp, ge, spa, lat, fr)
 val wpfs = listOf(inf, lat)
 
+val testFachdata = readDataStruct()
 
 val testKurswahl = KurswahlData().apply {
-    lk1 = faecher[0]
-    lk2 = faecher[1]
-    pf3 = faecher[2]
-    pf4 = faecher[3]
-    pf5 = faecher[4]
-    gks = faecher.associateWith { Wahlmoeglichkeit.DRITTES_VIERTES }
-    fremdsprachen = listOf(eng to 3, spa to 7)
+    lk1 = testFachdata.faecherMap["En"]!!
+    lk2 = testFachdata.faecherMap["Spa"]!!
+    gks = testFachdata.faecher.associateWith { Wahlmoeglichkeit.DRITTES_VIERTES }
+    fremdsprachen = listOf(testFachdata.faecherMap["En"]!! to 3, testFachdata.faecherMap["Spa"]!! to 7)
     wpfs = inf to null
 }
-
-//val testFachdata = FachData(faecher, fremdsprachen, wpfs, mapOf(ma to Wahlmoeglichkeit.DURCHGEHEND, de to Wahlmoeglichkeit.DURCHGEHEND, sp to Wahlmoeglichkeit.DURCHGEHEND), )
-val testFachdata = readDataStruct()
