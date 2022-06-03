8cf53274306b8a7187078d22cc4888dc0c51961a
heinrich26 <hendrik-horstmann@o2mail.de>
2022-06-03 10:41:33 +0200

    keine|durchgezogen Linientyp hinzugefügt und Wahlzeilen angepasst (siehe Wahlzeilen 34-45).

--- src/main/kotlin/data/WahlzeileLinientyp.kt
+++ src/main/kotlin/data/WahlzeileLinientyp.kt
@@ -5,5 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty
 enum class WahlzeileLinientyp {
     @JsonProperty("keine") KEINE,
     @JsonProperty("gestrichelt") GESTRICHELT,
+    @JsonProperty("keine|durchgezogen") KEINE_DURCHGEZOGEN,
     @JsonProperty("durchgezogen") DURCHGEZOGEN
 }
