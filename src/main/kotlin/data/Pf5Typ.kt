f0ec51616b183921735e420a61ec25c6057d3e03
henrich26 <hendrik-horstmann@o2mail.de>
2022-06-08 18:47:14 +0200

    Renderer hinzugefügt um in Comboboxen angezeigt zu werden

--- src/main/kotlin/data/Pf5Typ.kt
+++ src/main/kotlin/data/Pf5Typ.kt
@@ -1,9 +1,28 @@
 package data
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import java.awt.Component
+import javax.swing.DefaultListCellRenderer
+import javax.swing.JList
 
 enum class Pf5Typ(val repr: String) {
-    @JsonProperty("schriftl") SCHRIFTLICH("schriftl."),
-    @JsonProperty("praes") PRAESENTATION("Präs."),
-    @JsonProperty("wettbewerb") WETTBEWERB("Wettbewerb")
+    @JsonProperty("schriftl") SCHRIFTLICH("schriftlich"),
+    @JsonProperty("praes") PRAESENTATION("Präsentation"),
+    @JsonProperty("wettbewerb") WETTBEWERB("Wettbewerb");
+
+    object Renderer : DefaultListCellRenderer() {
+        override fun getListCellRendererComponent(
+            list: JList<*>?,
+            value: Any?,
+            index: Int,
+            isSelected: Boolean,
+            cellHasFocus: Boolean
+        ): Component? = super.getListCellRendererComponent(
+            list,
+            if (value is Pf5Typ) value.repr else "Ungesetzt",
+            index,
+            isSelected,
+            cellHasFocus
+        )
+    }
 }
