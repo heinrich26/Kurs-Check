58f96e825df6e8dba128788aa7d53f15ef2c75eb
heinrich26 <hendrik-horstmann@o2mail.de>
2022-06-10 01:34:52 +0200

    Docs verbessert/an Kotlin angepasst; aufgeräumt

--- src/main/kotlin/gui/ScrollablePanel.kt
+++ src/main/kotlin/gui/ScrollablePanel.kt
@@ -11,7 +11,7 @@ import javax.swing.SwingConstants
 
 
 /**
- * A panel that implements the Scrollable interface. This class allows you
+ * A panel that implements the [Scrollable] interface. This class allows you
  * to customize the scrollable features by using newly provided setter methods
  * so you don't have to extend this class every time.
  *
@@ -19,7 +19,7 @@ import javax.swing.SwingConstants
  * as an actual pixel value. The amount can be changed for both unit and block
  * scrolling for both horizontal and vertical scrollbars.
  *
- * The Scrollable interface only provides a boolean value for determining whether
+ * The [Scrollable] interface only provides a boolean value for determining whether
  * or not the viewport size (width or height) should be used by the scrollpane
  * when determining if scrollbars should be made visible. This class supports the
  * concept of dynamically changing this value based on the size of the viewport.
@@ -28,9 +28,10 @@ import javax.swing.SwingConstants
  * components added to the panel will be size to fill the area available,
  * based on the rules of the applicable layout manager of course.
  *
- * @see <a href="https://tips4java.wordpress.com/2009/12/20/scrollable-panel/">Original Source (copied)</a>
+ * from: [Original Source (copied)](https://tips4java.wordpress.com/2009/12/20/scrollable-panel/)
  */
-open class ScrollablePanel @JvmOverloads constructor(layout: LayoutManager? = FlowLayout()) : JPanel(layout),
+@Suppress("unused")
+open class ScrollablePanel(layout: LayoutManager? = FlowLayout()) : JPanel(layout),
     Scrollable, SwingConstants {
     enum class ScrollableSizeHint {
         NONE, FIT, STRETCH
@@ -48,9 +49,9 @@ open class ScrollablePanel @JvmOverloads constructor(layout: LayoutManager? = Fl
     private var verticalUnit: IncrementInfo? = null
 
     /**
-     * Get the height ScrollableSizeHint enum
+     * Get the height [ScrollableSizeHint] enum
      *
-     * @return the ScrollableSizeHint enum for the height
+     * @return the [ScrollableSizeHint] enum for the height
      */
     fun getScrollableHeight(): ScrollableSizeHint {
         return scrollableHeight
@@ -61,14 +62,14 @@ open class ScrollablePanel @JvmOverloads constructor(layout: LayoutManager? = Fl
      * determine the boolean value that is returned by the
      * getScrollableTracksViewportHeight() method. The valid values are:
      *
-     * ScrollableSizeHint.NONE - return "false", which causes the height
+     * - [ScrollableSizeHint.NONE] -> `false`, which causes the height
      * of the panel to be used when laying out the children
-     * ScrollableSizeHint.FIT - return "true", which causes the height of
+     * - [ScrollableSizeHint.FIT] -> `true`, which causes the height of
      * the viewport to be used when laying out the children
-     * ScrollableSizeHint.STRETCH - return "true" when the viewport height
-     * is greater than the height of the panel, "false" otherwise.
+     * - [ScrollableSizeHint.STRETCH] -> `true` when the viewport height
+     * is greater than the height of the panel, `false` otherwise.
      *
-     * @param scrollableHeight as represented by the ScrollableSizeHint enum.
+     * @param scrollableHeight as represented by the [ScrollableSizeHint] enum.
      */
     fun setScrollableHeight(scrollableHeight: ScrollableSizeHint) {
         this.scrollableHeight = scrollableHeight
@@ -76,27 +77,25 @@ open class ScrollablePanel @JvmOverloads constructor(layout: LayoutManager? = Fl
     }
 
     /**
-     * Get the width ScrollableSizeHint enum
+     * Get the width [ScrollableSizeHint] enum
      *
-     * @return the ScrollableSizeHint enum for the width
+     * @return the [ScrollableSizeHint] enum for the width
      */
-    fun getScrollableWidth(): ScrollableSizeHint {
-        return scrollableWidth
-    }
+    fun getScrollableWidth(): ScrollableSizeHint = scrollableWidth
 
     /**
-     * Set the ScrollableSizeHint enum for the width. The enum is used to
+     * Set the [ScrollableSizeHint] enum for the width. The enum is used to
      * determine the boolean value that is returned by the
-     * getScrollableTracksViewportWidth() method. The valid values are:
+     * [getScrollableTracksViewportWidth] method. The valid values are:
      *
-     * ScrollableSizeHint.NONE - return "false", which causes the width
+     * - [ScrollableSizeHint.NONE] -> `false`, which causes the width
      * of the panel to be used when laying out the children
-     * ScrollableSizeHint.FIT - return "true", which causes the width of
+     * - [ScrollableSizeHint.FIT] -> `true`, which causes the width of
      * the viewport to be used when laying out the children
-     * ScrollableSizeHint.STRETCH - return "true" when the viewport width
-     * is greater than the width of the panel, "false" otherwise.
+     * - [ScrollableSizeHint.STRETCH] -> `true` when the viewport width
+     * is greater than the width of the panel, `false` otherwise.
      *
-     * @param scrollableWidth as represented by the ScrollableSizeHint enum.
+     * @param scrollableWidth as represented by the [ScrollableSizeHint] enum.
      */
     fun setScrollableWidth(scrollableWidth: ScrollableSizeHint) {
         this.scrollableWidth = scrollableWidth
@@ -104,40 +103,38 @@ open class ScrollablePanel @JvmOverloads constructor(layout: LayoutManager? = Fl
     }
 
     /**
-     * Get the block IncrementInfo for the specified orientation
+     * Get the block [IncrementInfo] for the specified orientation
      *
-     * @return the block IncrementInfo for the specified orientation
+     * @return the block [IncrementInfo] for the specified orientation
      */
-    fun getScrollableBlockIncrement(orientation: Int): IncrementInfo? {
-        return if (orientation == SwingConstants.HORIZONTAL) horizontalBlock else verticalBlock
-    }
+    fun getScrollableBlockIncrement(orientation: Int): IncrementInfo? =
+        if (orientation == SwingConstants.HORIZONTAL) horizontalBlock else verticalBlock
 
     /**
      * Specify the information needed to do block scrolling.
      *
      * @param orientation  specify the scrolling orientation. Must be either:
-     * SwingContants.HORIZONTAL or SwingContants.VERTICAL.
-     * @paran type  specify how the amount parameter in the calculation of
-     * the scrollable amount. Valid values are:
-     * IncrementType.PERCENT - treat the amount as a % of the viewport size
-     * IncrementType.PIXEL - treat the amount as the scrollable amount
-     * @param amount  a value used with the IncrementType to determine the
+     * [SwingConstants.HORIZONTAL] or [SwingConstants.VERTICAL].
+     * @param type specify how the amount parameter in the calculation of
+     * the scrollable amount.Valid values are:
+     * - [IncrementType.PERCENT] - treat the amount as a % of the viewport size
+     * - [IncrementType.PIXELS] - treat the amount as the scrollable amount
+     * @param amount a value used with the [IncrementType] to determine the
      * scrollable amount
      */
     fun setScrollableBlockIncrement(orientation: Int, type: IncrementType, amount: Int) {
-        val info = IncrementInfo(type, amount)
-        setScrollableBlockIncrement(orientation, info)
+        setScrollableBlockIncrement(orientation, IncrementInfo(type, amount))
     }
 
     /**
      * Specify the information needed to do block scrolling.
      *
      * @param orientation  specify the scrolling orientation. Must be either:
-     * SwingContants.HORIZONTAL or SwingContants.VERTICAL.
-     * @param info  An IncrementInfo object containing information of how to
+     * [SwingConstants.HORIZONTAL] or [SwingConstants.VERTICAL].
+     * @param info An [IncrementInfo] object containing information on how to
      * calculate the scrollable amount.
      */
-    fun setScrollableBlockIncrement(orientation: Int, info: IncrementInfo?) {
+    private fun setScrollableBlockIncrement(orientation: Int, info: IncrementInfo?) {
         when (orientation) {
             SwingConstants.HORIZONTAL -> horizontalBlock = info
             SwingConstants.VERTICAL -> verticalBlock = info
@@ -146,24 +143,21 @@ open class ScrollablePanel @JvmOverloads constructor(layout: LayoutManager? = Fl
     }
 
     /**
-     * Get the unit IncrementInfo for the specified orientation
-     *
-     * @return the unit IncrementInfo for the specified orientation
+     * Get the unit [IncrementInfo] for the specified orientation
      */
-    fun getScrollableUnitIncrement(orientation: Int): IncrementInfo? {
-        return if (orientation == SwingConstants.HORIZONTAL) horizontalUnit else verticalUnit
-    }
+    fun getScrollableUnitIncrement(orientation: Int): IncrementInfo? =
+        if (orientation == SwingConstants.HORIZONTAL) horizontalUnit else verticalUnit
 
     /**
      * Specify the information needed to do unit scrolling.
      *
-     * @param orientation  specify the scrolling orientation. Must be either:
-     * SwingContants.HORIZONTAL or SwingContants.VERTICAL.
-     * @paran type  specify how the amount parameter in the calculation of
+     * @param orientation specify the scrolling orientation. Must be either:
+     * [SwingConstants.HORIZONTAL] or [SwingConstants.VERTICAL].
+     * @param type specify how the amount parameter in the calculation of
      * the scrollable amount. Valid values are:
-     * IncrementType.PERCENT - treat the amount as a % of the viewport size
-     * IncrementType.PIXEL - treat the amount as the scrollable amount
-     * @param amount  a value used with the IncrementType to determine the
+     * - [IncrementType.PERCENT] - treat the amount as a % of the viewport size
+     * - [IncrementType.PIXELS] - treat the amount as the scrollable amount
+     * @param amount a value used with the [IncrementType] to determine the
      * scrollable amount
      */
     fun setScrollableUnitIncrement(orientation: Int, type: IncrementType, amount: Int) {
@@ -174,12 +168,12 @@ open class ScrollablePanel @JvmOverloads constructor(layout: LayoutManager? = Fl
     /**
      * Specify the information needed to do unit scrolling.
      *
-     * @param orientation  specify the scrolling orientation. Must be either:
-     * SwingContants.HORIZONTAL or SwingContants.VERTICAL.
-     * @param info  An IncrementInfo object containing information of how to
+     * @param orientation specify the scrolling orientation. Must be either:
+     * [SwingConstants.HORIZONTAL] or [SwingConstants.VERTICAL].
+     * @param info  An [IncrementInfo] object containing information of how to
      * calculate the scrollable amount.
      */
-    fun setScrollableUnitIncrement(orientation: Int, info: IncrementInfo?) {
+    private fun setScrollableUnitIncrement(orientation: Int, info: IncrementInfo?) {
         when (orientation) {
             SwingConstants.HORIZONTAL -> horizontalUnit = info
             SwingConstants.VERTICAL -> verticalUnit = info
@@ -188,9 +182,7 @@ open class ScrollablePanel @JvmOverloads constructor(layout: LayoutManager? = Fl
     }
 
     //  Implement Scrollable interface
-    override fun getPreferredScrollableViewportSize(): Dimension {
-        return preferredSize
-    }
+    override fun getPreferredScrollableViewportSize(): Dimension = preferredSize
 
     override fun getScrollableUnitIncrement(
         visible: Rectangle, orientation: Int, direction: Int
@@ -204,17 +196,14 @@ open class ScrollablePanel @JvmOverloads constructor(layout: LayoutManager? = Fl
 
     override fun getScrollableBlockIncrement(
         visible: Rectangle, orientation: Int, direction: Int
-    ): Int {
-        return when (orientation) {
-            SwingConstants.HORIZONTAL -> getScrollableIncrement(horizontalBlock, visible.width)
-            SwingConstants.VERTICAL -> getScrollableIncrement(verticalBlock, visible.height)
-            else -> throw IllegalArgumentException("Invalid orientation: $orientation")
-        }
+    ): Int = when (orientation) {
+        SwingConstants.HORIZONTAL -> getScrollableIncrement(horizontalBlock, visible.width)
+        SwingConstants.VERTICAL -> getScrollableIncrement(verticalBlock, visible.height)
+        else -> throw IllegalArgumentException("Invalid orientation: $orientation")
     }
 
-    protected fun getScrollableIncrement(info: IncrementInfo?, distance: Int): Int {
-        return if (info!!.increment == IncrementType.PIXELS) info.amount else distance * info.amount / 100
-    }
+    private fun getScrollableIncrement(info: IncrementInfo?, distance: Int): Int =
+        if (info!!.increment == IncrementType.PIXELS) info.amount else distance * info.amount / 100
 
     override fun getScrollableTracksViewportWidth(): Boolean {
         if (scrollableWidth == ScrollableSizeHint.NONE) return false
@@ -241,20 +230,9 @@ open class ScrollablePanel @JvmOverloads constructor(layout: LayoutManager? = Fl
      */
     class IncrementInfo(val increment: IncrementType, val amount: Int) {
 
-        override fun toString(): String {
-            return "ScrollablePanel[" +
-                    increment + ", " +
-                    amount + "]"
-        }
+        override fun toString(): String = "ScrollablePanel[$increment, $amount]"
     }
-    /**
-     * Constuctor for specifying the LayoutManager of the panel.
-     *
-     * @param layout the LayountManger for the panel
-     */
-    /**
-     * Default constructor that uses a FlowLayout
-     */
+
     init {
         val block = IncrementInfo(IncrementType.PERCENT, 100)
         val unit = IncrementInfo(IncrementType.PERCENT, 10)
