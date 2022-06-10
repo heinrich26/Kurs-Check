ae63dc36dc9996ba97054b7ab7520c1639607ab0
heinrich26 <hendrik-horstmann@o2mail.de>
2022-06-10 13:44:11 +0200

    Testdaten werden nun aus `test_wahl.kurswahl` gelesen

--- src/main/kotlin/TestData.kt
+++ src/main/kotlin/TestData.kt
@@ -1,28 +1,7 @@
-import data.Fach
-import data.KurswahlData
-import data.Wahlmoeglichkeit
 
-val eng = Fach("Englisch", "En", 1, true)
-val ma = Fach("Mathe", "Ma", 2, true)
-val de = Fach("Deutsch", "De", 1, true)
-val inf = Fach("Informatik", "Inf", 2, true)
-val geo = Fach("Geografie", "Geo", 3, true)
-val sp = Fach("Sport", "Sp", 0, false)
-val ge = Fach("Geschichte", "Ge", 3, true)
-val spa = Fach("Spanisch", "Spa", 1, true)
-val lat = Fach("Latein", "Lat", 1, false)
-val fr = Fach("Französisch", "Fr", 1, true)
-
-val fremdsprachen = listOf(eng, spa, lat, fr)
-var faecher = listOf(eng, ma, de, inf, geo, sp, ge, spa, lat, fr)
-val wpfs = listOf(inf, lat)
+import gui.Consts
+import java.io.File
 
 val testFachdata = readDataStruct()
 
-val testKurswahl = KurswahlData().apply {
-    lk1 = testFachdata.faecherMap["En"]!!
-    lk2 = testFachdata.faecherMap["Spa"]!!
-    gks = testFachdata.faecher.associateWith { Wahlmoeglichkeit.DRITTES_VIERTES }
-    fremdsprachen = listOf(testFachdata.faecherMap["En"]!! to 3, testFachdata.faecherMap["Spa"]!! to 7)
-    wpfs = inf to null
-}
+val testKurswahl = testFachdata.loadKurswahl(File(getResource(Consts.TEST_FILE_NAME)!!))
